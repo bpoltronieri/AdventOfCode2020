a380668d156033fff9e56e60a4bd3c706b79e1fc
Bruno Poltronieri <brunopoltronieri@hotmail.com>
2020-12-01 00:59:51 +0000

    Add dummy unit test for day 1.

---
+++ test/AoC2020.Tests/Days/Day01Tests.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace AoC2020.Tests.Days
+{
+    public class Day01Tests
+    {
+        [Fact]
+        public void Day01Test1()
+        {
+            // arrange
+            // get test input file
+
+            // act
+            // var day01 = new Day01(testInputFile1);
+            // var result1 = day01.PartOne();
+            // var result2 = day01.PartTwo();
+
+            // assert
+            // Assert.Equal(result1, "xyz");
+            // Assert.Equal(result2, "abc");
+
+            Assert.Equal('A', 'A');
+        }
+    }
+}
