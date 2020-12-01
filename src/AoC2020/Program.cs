9b1aad646609f45943192ab908ba24a99aa5ca10
Bruno Poltronieri <brunopoltronieri@hotmail.com>
2020-12-01 02:27:00 +0000

    Implement console interface.

--- src/AoC2020/Program.cs
+++ src/AoC2020/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace AoC2020
 {
@@ -6,7 +7,62 @@ namespace AoC2020
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var day = 0;
+            while (day == 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Enter day: ");
+                var command = Console.ReadLine();
+
+                if (!int.TryParse(command, out day) || day < 1 || day > 25)
+                    {
+                        day = 0;
+                        Console.WriteLine("Invalid Day");
+                    }
+            }
+
+            IDay solution;
+            try
+            {
+                var dayName = $"Day{day:d2}";
+                var typeName = "AoC2020.Days." + dayName;
+                var dayType = Type.GetType(typeName);
+
+                var inputFile = Directory.GetFiles(@"../../Input", dayName + ".txt")[0];
+
+                solution = (IDay)Activator.CreateInstance(dayType, inputFile);
+            }
+            catch
+            {
+                Console.WriteLine("Error loading solution");
+                return;
+            }
+
+            var part = 0;
+            while (part == 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Which part would you like to solve?");
+                var command = Console.ReadLine();
+
+                if (!int.TryParse(command, out part) || (part != 1 && part != 2))
+                    {
+                        part = 0;
+                        Console.WriteLine("Invalid Part");
+                    }
+            }
+
+            string answer = "";
+            switch (part)
+            {
+                case 1:
+                    answer = solution.PartOne();
+                    break;
+                case 2:
+                    answer = solution.PartTwo();
+                    break;
+            }
+            Console.WriteLine(answer);
         }
     }
 }
